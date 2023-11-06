273f486563102f3a0300bd5d4ba177c4ff750d8f
xogoodnow <xo4ever9999@gmail.com>
2023-11-06 18:25:50 +0330

    The templating issue was fixed

--- Terraform/modules/servers/etchost.tpl
+++ Terraform/modules/servers/etchost.tpl
@@ -1,15 +1,12 @@
 127.0.0.1 localhost
 
 %{ for index, ip in master_ips }
-${ip} master-0
-${ip} master-1
-${ip} master-2
+${ip} master-${index}
 %{ endfor }
 
 
-%{ for index, ip in haproxy_ip }
-${ip} control.oblivionone.com
-%{ endfor }
+${haproxy_ip} control.oblivionone.com
+
 
 ff02::1 ip6-allnodes
 ff02::2 ip6-allrouters
--- Terraform/modules/servers/main.tf
+++ Terraform/modules/servers/main.tf
@@ -62,10 +62,84 @@ resource "local_file" "inventory" {
 resource "local_file" "etcd-hosts" {
   content  = templatefile("${path.module}/etchost.tpl",
     {
-      master_0_private_ips = hcloud_server.master.0.network[*].ip
-      master_1_private_ips = hcloud_server.master.1.network[*].ip
-      master_2_private_ips = hcloud_server.master.2.network[*].ip
-      haproxy_private_ips = hcloud_server.haproxy.network[*].ip
+      master_ips = hcloud_server.master.*.ipv4_address
+      haproxy_ip = hcloud_server.haproxy.ipv4_address
+resource "hcloud_server" "master" {
+  count = 3
+  name         = "master-${count.index}"
+  image        = var.image_name
+  server_type  = "cpx31"
+  ssh_keys = [data.hcloud_ssh_key.key1.id,data.hcloud_ssh_key.key2.id,data.hcloud_ssh_key.key3.id]
+  location = var.location
+  network {
+    network_id = data.hcloud_network.private-network.id
+  }
+
+}
+
+
+
+resource "hcloud_server" "haproxy" {
+  name         = "haproxy"
+  image        = var.image_name
+  server_type  = "cx21"
+  ssh_keys = [data.hcloud_ssh_key.key1.id,data.hcloud_ssh_key.key2.id,data.hcloud_ssh_key.key3.id]
+  location = var.location
+  network {
+    network_id = data.hcloud_network.private-network.id
+  }
+}
+
+
+data "hcloud_network" "private-network" {
+  name = "k8s-internal"
+
+}
+
+
+data "hcloud_ssh_key" "key1"  {
+  name = "kang"
+
+}
+
+data "hcloud_ssh_key" "key2"  {
+  name = "Kangkey"
+
+}
+
+data "hcloud_ssh_key" "key3" {
+  name = "ssh_key_bastion"
+
+}
+
+
+resource "local_file" "inventory" {
+  content = templatefile("${path.module}/inventory.tpl",
+    {
+      master_ips = hcloud_server.master.*.ipv4_address
+      haproxy_ip = hcloud_server.haproxy.ipv4_address
+    }
+  )
+  filename = "${path.module}/../../inventory.yaml"
+}
+
+
+
+resource "local_file" "etcd-hosts" {
+  content  = templatefile("${path.module}/etchost.tpl",
+    {
+      master_ips = hcloud_server.master.*.ipv4_address
+      haproxy_ip = hcloud_server.haproxy.ipv4_address
+
+    }
+  )
+  filename = "${path.module}/../../Ansible/roles/Pre_Setup/files/etchost.yaml"
+}
+
+
+
+
+
 
     }
   )
