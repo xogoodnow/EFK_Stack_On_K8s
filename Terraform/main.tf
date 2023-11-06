db43c6ed7d8c56d3089b16b98387890431d7f8f5
xogoodnow <xo4ever9999@gmail.com>
2023-11-06 15:29:30 +0330

    The basis of terraform is completed

---
+++ Terraform/main.tf
@@ -0,0 +1,36 @@
+module "ssh_keys" {
+  source = "./modules/ssh_keys"
+  hcloud_token = var.hcloud_token
+}
+
+module "Private_Network" {
+  source = "./modules/Private_Network"
+  hcloud_token = var.hcloud_token
+  depends_on   = [module.ssh_keys]
+
+}
+
+
+module "servers" {
+  source       = "./modules/servers"
+  hcloud_token = var.hcloud_token
+  image_name   = var.image_name
+  server_type  = var.server_type
+  location     = var.location
+  depends_on   = [module.Private_Network]
+}
+
+
+module "volume" {
+  source = "./modules/volume"
+  hcloud_token = var.hcloud_token
+  depends_on = [module.servers]
+
+}
+
+
+module "k8s" {
+  source = "./modules/Gitlab"
+  hcloud_token = var.hcloud_token
+  depends_on = [module.volume]
+}
